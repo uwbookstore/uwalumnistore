f786180b059a9c18a492d1ed6d8b5b7cd83b57b9
ClaytonDewey <clay@dryadmedia.com>
2023-10-05 15:22:44 -0500

    Reorganized files since BS5 upgrade is done

---
+++ js/registration.js
@@ -0,0 +1,4 @@
+const cTS = document.getElementById('customerTypeSelect');
+let alumniType = '227';
+
+!function(){var e=document.getElementById("campaign-embed-code");e||((e=document.createElement("SCRIPT")).src=https://d1m2uzvk8r2fcn.cloudfront.net/scripts/embed-code/20230927/embed.min.js,e.id="campaign-embed-code",document.body.appendChild(e))}()
